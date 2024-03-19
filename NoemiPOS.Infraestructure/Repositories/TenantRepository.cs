e7a0d19afdb8f632783e5d6a82f30f745ac1197d
Hector Caballero <caballero.22.809@gmail.com>
2024-03-18 18:19:30 -0600

    Add TenantRepository

---
+++ NoemiPOS.Infraestructure/Repositories/TenantRepository.cs
@@ -0,0 +1,9 @@
+﻿using NoemiPOS.Domain.Tenants;
+
+namespace NoemiPOS.Infraestructure.Repositories;
+internal sealed class TenantRepository : Repository<Tenant>, ITenantRepository
+{
+    public TenantRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {         
+    }
+}
