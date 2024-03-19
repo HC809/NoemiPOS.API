352c079e5ee17ab06ba0300bacfac2b9d2516354
Hector Caballero <caballero.22.809@gmail.com>
2024-03-18 21:27:46 -0600

    Add ApplicationBuilderExtensions

---
+++ NoemiPOS.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NoemiPOS.Infraestructure;
+
+namespace NoemiPOS.Api.Extensions;
+
+internal static class ApplicationBuilderExtensions
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
--- NoemiPOS.Api/Program.cs
+++ NoemiPOS.Api/Program.cs
@@ -1,19 +1,24 @@
+using NoemiPOS.Api.Extensions;
+using NoemiPOS.Application;
+using NoemiPOS.Infraestructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
+builder.Services.AddInfraestructure(builder.Configuration);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
