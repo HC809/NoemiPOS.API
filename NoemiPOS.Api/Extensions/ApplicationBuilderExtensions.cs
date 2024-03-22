eabeaac5306dfe1e9fdda895770466700294da0a
Hector Caballero <caballero.22.809@gmail.com>
2024-03-22 16:13:40 -0600

    Add ExceptionHandlingMiddleware in API

--- NoemiPOS.Api/Extensions/ApplicationBuilderExtensions.cs
+++ NoemiPOS.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NoemiPOS.Api.Middleware;
 using NoemiPOS.Infraestructure;
 
 namespace NoemiPOS.Api.Extensions;
@@ -10,5 +11,10 @@ internal static class ApplicationBuilderExtensions
         using var scope = app.ApplicationServices.CreateScope();
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
         dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
 }
--- NoemiPOS.Api/Program.cs
+++ NoemiPOS.Api/Program.cs
@@ -23,7 +23,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCustomExceptionHandler();
 
 app.MapControllers();
 
